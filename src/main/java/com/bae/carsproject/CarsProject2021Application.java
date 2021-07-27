a1d6d5b8bde2a8d18428364383cf4175ca66e79b
ShamReh <sr.166@hotmail.co.uk>
2021-07-27 11:36:48 +0100

    Initial App

---
+++ src/main/java/com/bae/carsproject/CarsProject2021Application.java
@@ -0,0 +1,13 @@
+package com.bae.carsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsProject2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsProject2021Application.class, args);
+	}
+
+}
