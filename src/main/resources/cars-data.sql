7661b18b4a7067244916886ebfaa847f871e6748
ShamReh <sr.166@hotmail.co.uk>
2021-07-28 10:16:36 +0100

    Testing

---
+++ src/main/resources/cars-data.sql
@@ -0,0 +1,9 @@
+insert into `cars` 
+
+	(`age`, `brand`, `model`) 
+	
+values 
+
+	(2019, 'Audi', 'RS6'),
+	(2020, 'Ferrari', '488'),
+	(2016,'McLaren', '750LT Spider');
--- src/main/resources/cars1-data.sql
+++
@@ -1,9 +0,0 @@
-insert into `cars1` 
-
-	(`age`, `brand`, `model`) 
-	
-values 
-
-	(2019, 'BMW', 'M4'),
-	(2020, 'Ferrari', '458'),
-	(2016,'McLaren', '650S');
